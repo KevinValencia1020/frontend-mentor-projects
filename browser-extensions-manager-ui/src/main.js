47f5faffc257bca509017f45e0b19c30d41a7086
Kevin valencia <kevintovarvalencia21@gmail.com>
2025-11-12 21:49:59 -0500

    feat(browser-extensions-manager-ui): importo y renderizo el componente de Extensiones en el main

--- browser-extensions-manager-ui/src/main.js
+++ browser-extensions-manager-ui/src/main.js
@@ -1,8 +1,10 @@
 import './style.css'
 import Header from './components/header.js';
+import Extensions from './components/Extensions.js';
 
 const template = `
   ${Header()}
+  ${Extensions()}
 `;
 
 const app = document.getElementById('app');
